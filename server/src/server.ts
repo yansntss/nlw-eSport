7619efb4b305dbadfc2cfa82570ed06797c89c17
Yan Santos <yansantos.dev@gmail.com>
2022-09-16 18:54:09 -0300

    Create server.ts

---
+++ server/src/server.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+
+
+const app = express();
+const prisma = new PrismaClient({
+  log: ['query']
+});
+
+app.get('/games', async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true
+        }
+      }
+    }
+  });
+
+  return res.json(games);
+
+})
+
+app.get('/games/:id/ids', async (req, res) => {
+  const gameId = req.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hoursStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId
+    }
+  })
+  return res.json(ads);
+
+
+}) 
+
+app.listen(3333)
